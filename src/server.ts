216c36d8acc2596236a9d411d77ccc44090cf033
Erik <erik.p.schmitt@gmail.com>
2023-12-14 21:08:45 +0100

    Added logger.

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,7 @@ import { configureCors } from './startup/cors';
 import isOnline from './routes/base';
 import addRateLimiter from './startup/limitRate';
 import * as dotenv from 'dotenv';
+import logger from './utils/logger';
 dotenv.config();
 
 const app: Express = express();
@@ -15,6 +16,8 @@ app.use(express.json());
 app.use('/', isOnline);
 
 const port: number = process.env.PORT ? parseInt(process.env.PORT) : 3000;
-const server = app.listen(port, '0.0.0.0');
+const server = app.listen(port, '0.0.0.0', () => {
+    logger.info('Server started on port: ' + port);
+});
 
 export { server };
